e4ecedb2c95e54de7788d1eb971ee7985bb2a5ba
guiixta <ferreiragui@proton.me>
2025-12-12 20:02:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd;
+import dash
+from dash import Dash, dcc, html
+import plotly.graph_objects as go
+
+
+
+def load_dataBase():
+    df = pd.read_csv('');
+    return df;
+
+df = load_dataBase();
+
+linksExternos = ['https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4']
+
+
+app = Dash(__name__, external_scripts=linksExternos, title="O Fiscal da Câmara")
+
+
+app.layout = html.Div([
+    html.H4('Interactive color selection with simple Dash example'),
+    html.P("Select color:"),
+    dcc.Dropdown(
+        id="dropdown",
+        options=['Gold', 'MediumTurquoise', 'LightGreen'],
+        value='Gold',
+        clearable=False,
+   )
+])
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
